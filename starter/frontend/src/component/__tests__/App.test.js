cc5892ec8063303b5b00471614dbdbf2ac5d6063
Orupalli.Srilatha <siriorupalli4@gmail.com>
2026-09-09 14:19:00 +0530

    Add test for Movie List heading in App component
    Add a test to verify the Movie List heading is rendered

--- starter/frontend/src/component/__tests__/App.test.js
+++ starter/frontend/src/component/__tests__/App.test.js
@@ -1,1 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import App from '../../App';
 
+test('renders Movie List heading', () => {
+  render(<App />);
+  const heading = screen.getByText('Movie List');
+  expect(heading).toBeInTheDocument();
+});
