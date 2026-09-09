a06982a817ac605fe5765ab1e0a0a0700bc36a78
Orupalli.Srilatha <siriorupalli4@gmail.com>
2026-09-09 14:20:45 +0530

    Add test for MovieList component
    Added a test for rendering movie titles using mocked axios response.

--- starter/frontend/src/component/__tests__/movieList.test.js
+++ starter/frontend/src/component/__tests__/movieList.test.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+import MovieList from '../MovieList';
 
+jest.mock('axios');
+
+const movies = [
+  { id: 1, title: 'Movie 1' },
+  { id: 2, title: 'Movie 2' },
+];
+
+test('renders movie titles', async () => {
+  axios.get.mockResolvedValueOnce({
+    data: { movies },
+  });
+
+  render(<MovieList onMovieClick={() => {}} />);
+
+  expect(await screen.findByText('Movie 1')).toBeInTheDocument();
+  expect(await screen.findByText('Movie 2')).toBeInTheDocument();
+});
